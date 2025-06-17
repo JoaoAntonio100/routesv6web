9046277fdc434c9770c200de6db84fe6f5c4a7fb
João Antonio <joao.zdx@gmail.com>
2025-06-16 22:33:14 -0300

    Atualização modal

--- src/routes/PaginaModal.jsx
+++ src/routes/PaginaModal.jsx
@@ -12,40 +12,37 @@ function MeuBotaoModal() {
   const handleShow = () => setShow(true);
 
   return (
-    <Container>
-      <Row>
-        <Col sm={8}>Buscar</Col>
-        <Col sm={4}>
-        <Button
-            variant="primary"
-            onClick={handleShow}
-            style={{ float: 'right', marginTop: '10px' }}
-              >
-                   +
-        </Button>
-
-        </Col>
+<Container>
+   <Row>
+    <Col sm={8}>Buscar</Col>
+    <Col sm={4}>
+    <Button
+         variant="primary"
+        onClick={handleShow}
+        style={{ float: 'right', marginTop: '10px' }}
+          >
+             +
+    </Button>
+      </Col>
       </Row>
-
       <Row>
-        <Col>Tabela</Col>
+       <Col>Tabela</Col>
       </Row>
-
-      <Modal show={show} onHide={handleClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Opa, vc esta lendo um texto do modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </Container>
+  <Modal show={show} onHide={handleClose} centered>
+    <Modal.Header closeButton>
+       <Modal.Title>Modal heading</Modal.Title>
+     </Modal.Header>
+     <Modal.Body>Opa, vc esta lendo um texto do modal!</Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>
+         Close
+       </Button>
+       <Button variant="primary" onClick={handleClose}>
+         Save Changes
+      </Button>
+    </Modal.Footer>
+    </Modal>
+</Container>
   );
 }
 
